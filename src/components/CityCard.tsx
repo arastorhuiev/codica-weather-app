96b58f0c870e863b9a1fb648b3b8e9e0db424b38
Andrii Rastorhuiev <arastorhuiev@gmail.com>
2022-12-12 15:44:31 +0200

    refactor

--- src/components/CityCard.tsx
+++ src/components/CityCard.tsx
@@ -1,81 +1,55 @@
-import { useMemo } from 'react';
-
-import { useAppSelector } from '../store/hooks';
-
 import { Box, Button, Grid, Typography } from '@mui/material';
+import { FC } from 'react';
+import { IWeather } from '../store/types';
 
-export function CityCard() {
-  const citiesData = useAppSelector((state) => state.weatherCity.data);
-  const dataStatus = useAppSelector((state) => state.weatherCity.status);
+type CityCardProps = {
+  cityData: IWeather;
+};
 
+export const CityCard: FC<CityCardProps> = ({ cityData }) => {
   const handleClickByCityName = (city: string): void => {
     console.log('hi');
   };
 
-  const dataCompleted = useMemo(
-    () =>
-      citiesData.map((cityData) => (
-        <Box
-          key={cityData.id}
-          sx={{
-            width: 360,
-            height: 300,
-            margin: 1,
-            backgroundColor: 'primary.dark',
-            borderRadius: 10,
-            '&:hover': {
-              opacity: 0.9,
-            },
-          }}>
-          <Grid
-            container
-            direction='column'
-            justifyContent='center'
-            alignItems='center'>
-            <Typography variant='body2'>{cityData.name}</Typography>
-            <Typography variant='body2'>{cityData.weather[0].main}</Typography>
-            <Typography variant='body2'>
-              {cityData.weather[0].description}
-            </Typography>
-            <Typography variant='body2'>temp: {cityData.main.temp}</Typography>
-            <Typography variant='body2'>
-              pressure: {cityData.main.pressure}
-            </Typography>
-            <Typography variant='body2'>
-              humidity: {cityData.main.humidity}
-            </Typography>
-            <Typography variant='body2'>
-              speed wind: {cityData.wind.speed}
-            </Typography>
-            <Button
-              variant='contained'
-              color='success'
-              onClick={() => handleClickByCityName(cityData.name)}>
-              Reload
-            </Button>
-          </Grid>
-        </Box>
-      )),
-    [citiesData],
-  );
-
-  const renderData = () => {
-    if (dataStatus === 'loading') {
-      return <></>;
-    }
-
-    if (dataStatus === 'error') {
-      return <></>;
-    }
-
-    if (dataStatus === 'completed') {
-      return dataCompleted;
-    }
-  };
-
   return (
-    <Grid container justifyContent='center'>
-      {renderData()}
-    </Grid>
+    <Box
+      sx={{
+        width: 360,
+        height: 300,
+        margin: 1,
+        backgroundColor: 'primary.dark',
+        borderRadius: 10,
+        '&:hover': {
+          opacity: 0.9,
+        },
+      }}>
+      <Grid
+        container
+        direction='column'
+        justifyContent='center'
+        alignItems='center'>
+        <Typography variant='body2'>{cityData.name}</Typography>
+        <Typography variant='body2'>{cityData.weather[0].main}</Typography>
+        <Typography variant='body2'>
+          {cityData.weather[0].description}
+        </Typography>
+        <Typography variant='body2'>temp: {cityData.main.temp}</Typography>
+        <Typography variant='body2'>
+          pressure: {cityData.main.pressure}
+        </Typography>
+        <Typography variant='body2'>
+          humidity: {cityData.main.humidity}
+        </Typography>
+        <Typography variant='body2'>
+          speed wind: {cityData.wind.speed}
+        </Typography>
+        <Button
+          variant='contained'
+          color='success'
+          onClick={() => handleClickByCityName(cityData.name)}>
+          Reload
+        </Button>
+      </Grid>
+    </Box>
   );
-}
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,42 @@
 import { CityCard } from '../components/CityCard';
 import { Search } from '../components/Search';
-import { Container, Stack } from '@mui/material';
+import { Container, Grid, Stack } from '@mui/material';
+import { useAppSelector } from '../store/hooks';
+import { useMemo } from 'react';
 
 export function Home() {
+  const citiesData = useAppSelector((state) => state.weatherCity.data);
+  const dataStatus = useAppSelector((state) => state.weatherCity.status);
+
+  const dataCompleted = useMemo(
+    () =>
+      citiesData.map((cityData) => (
+        <CityCard key={cityData.id} cityData={cityData} />
+      )),
+    [citiesData],
+  );
+
+  const renderData = () => {
+    if (dataStatus === 'loading') {
+      return <></>;
+    }
+
+    if (dataStatus === 'error') {
+      return <></>;
+    }
+
+    if (dataStatus === 'completed') {
+      return dataCompleted;
+    }
+  };
+
   return (
     <Container>
       <Stack spacing={2}>
         <Search />
-        <CityCard />
+        <Grid container justifyContent='center'>
+          {renderData()}
+        </Grid>
       </Stack>
     </Container>
   );
